bff4c6711412317f7bf0e5b94bd9089782b3101e
Matheus "Donuts <81537097+matheuscn16@users.noreply.github.com>
2023-04-17 17:37:27 -0300

    Add files via upload
    Front End

---
+++ telalogin.py
@@ -0,0 +1,20 @@
+from PyQt5 import uic,QtWidgets
+
+def funcao_principal():
+    linha1 = telalogin.lineEdit.text()
+    linha2 = telalogin.lineEdit_3.text()
+    print("id usuário:",linha1) 
+    print("Senha:",linha2)
+
+    if telalogin.radioButton.isChecked() :
+        print("Você está logado Professor")
+    else:
+          print("Você está logado Estudante")
+
+
+app=QtWidgets.QApplication([])
+telalogin=uic.loadUi("telalogin.ui")
+telalogin.pushButton.clicked.connect(funcao_principal)
+
+telalogin.show()
+app.exec()
